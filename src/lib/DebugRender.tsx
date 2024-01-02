683d6d50584366e902bfe348b7379573414b8ad9
wangxiang <forhot2000@qq.com>
2024-01-02 21:09:46 +0800

    suppressHydrationWarning

--- src/lib/DebugRender.tsx
+++ src/lib/DebugRender.tsx
@@ -1,28 +1,17 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useMemo } from 'react';
 
 export function DebugRender() {
   const path = usePathname();
-  const [createTime, updateTime] = useCreateAndUpdateTime();
+  const createTime = useMemo(() => new Date().toISOString(), []);
+  const updateTime = new Date().toISOString();
   return (
     <div className='debug'>
       <div>path: {path}</div>
-      <div>created: {createTime}</div>
-      <div>updated: {updateTime}</div>
+      <div suppressHydrationWarning>created: {createTime}</div>
+      <div suppressHydrationWarning>updated: {updateTime}</div>
     </div>
   );
-}
-
-function useCreateAndUpdateTime() {
-  const [createTime, setCreateTime] = useState('');
-  const [updateTime, setUpdateTime] = useState('');
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(() => {
-    const now = new Date().toISOString();
-    setCreateTime((prev) => prev || now);
-    setUpdateTime(now);
-  });
-  return [createTime, updateTime] as const;
 }
