68094455ac1cc4da6e283fb5b01fa8e0fad76127
wangxiang <forhot2000@qq.com>
2024-01-03 17:28:06 +0800

    modify swr

--- src/lib/swr.tsx
+++ src/lib/swr.tsx
@@ -1,14 +1,12 @@
-import useSWR_1, { BareFetcher, SWRHook } from 'swr';
+import origin, { SWRHook } from 'swr';
 
-const useSWR: typeof useSWR_1 = (key: any, fn?: any, opts?: any) => {
-  return useSWR_1(key, fn, {
-    ...opts,
-    use: [logger],
-  });
+const useSWR: SWRHook = (key: any, fn?: any, config?: any) => {
+  const { use = [] } = config || {};
+  return origin(key, fn, { ...config, use: [logger, ...use] });
 };
 
-function logger(useSWRNext: SWRHook) {
-  return (key: any, fetcher: BareFetcher, config: any) => {
+function logger(useSWRNext: any) {
+  return (key: any, fetcher: any, config: any) => {
     // 将日志记录器添加到原始 fetcher。
     const extendedFetcher = (...args: any[]) => {
       console.log('SWR Request:', key);
